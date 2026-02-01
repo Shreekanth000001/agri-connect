06166112f16c6b460d60481a119677eb1cd28373
Shreekanth.k <Shreekanth.k000001@gmail.com>
2026-02-01 11:01:38 +0530

    server client data id api

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -1,26 +1,17 @@
 "use client"
 import { useState, useEffect } from "react";
 import { useFormatter } from 'next-intl';
+import { useSearchParams } from 'next/navigation'
 import Link from "next/link";
 
-export default function Home({ params }: { params: { id: string } }) {
+export default function Home(props: any) {
   const format = useFormatter();
+  const searchParams = useSearchParams()
   const [proddata, setprodData] = useState([])
   const [fdata, setfData] = useState([])
-  function bringData() {
-    fetch(`http://localhost:3000/productListings/${params.id}`).then(res => res.json().then(data => {
-      setprodData(data.prodData);
-      setfData(data.fData);
-    }))
-  }
-  function dateFormat(date: string) {
-    const dateTime = new Date(date);
-    const newdate = format.dateTime(dateTime, {
-      day: 'numeric',
-      month: 'short',
-      year: 'numeric'
-    })
-    return newdate;
+  async function bringData() {
+    const id = searchParams.get('id')
+    console.log(id)
   }
   useEffect(() => {
     bringData()
@@ -29,7 +20,7 @@ export default function Home({ params }: { params: { id: string } }) {
     <div className="grow">
       {/* <button className="hover:bg-white" onClick={buttonAct}>Send</button> */}
       <div className="p-6 ml-3.5">
-        <p className="text-3xl font-semibold">{ fdata.uname}</p>
+        <p className="text-3xl font-semibold">{fdata.uname}</p>
         <p className="text-2xl font-semibold">Fruits</p>
         <div className="md:grid md:grid-cols-4">
         </div>
--- app/productListings/route.ts
+++ app/productListings/route.ts
@@ -1,30 +1,30 @@
+  "use server"
 import { prisma } from '@/lib//prisma';
-export async function GET(request: Request,
-  props: { params: Promise<{ id: string }> }
-) {
-  const params = await props.params;
-  const id = params.id;
-    const proddata = await prisma.productAuction.findUnique({ where: { ProdAucId: Number(id) } });
-    const fdata = await prisma.user.findUnique({ where: { uid: Number(proddata?.fid) } });
-    const combinedData = {
-        prodData:proddata,
-        fData:fdata
-    }
-    const returns= new Response(JSON.stringify(combinedData));
-    return returns;
+export async function POST( req: Request) {
+  const { id } = await req.json();
+  console.log("id recieved: ",id);
+
+  const proddata = await prisma.productAuction.findUnique({ where: { ProdAucId: Number(id) } });
+  const fdata = await prisma.user.findUnique({ where: { uid: Number(proddata?.fid) } });
+  const combinedData = {
+    prodData: proddata,
+    fData: fdata
+  }
+  const returns = new Response(JSON.stringify(combinedData));
+  return returns;
 }
 
-export async function POST() {
-    let bid = await prisma.productAuction.create({
-    data: {
-      fid: 2,
-      title: 'Banana',
-      description: 'Medium size',
-      startingBid: 80,
-      endTime: new Date("2026-01-27")
-    }
-  })
+// export async function POST() {
+//   let bid = await prisma.productAuction.create({
+//     data: {
+//       fid: 2,
+//       title: 'Banana',
+//       description: 'Medium size',
+//       startingBid: 80,
+//       endTime: new Date("2026-01-27")
+//     }
+//   })
 
-    return new Response(JSON.stringify(bid));
-}
+//   return new Response(JSON.stringify(bid));
+// }
 
