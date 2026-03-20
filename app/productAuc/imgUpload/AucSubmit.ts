8d7e031a3609ca5a2d63117b0dabba7c524c09fb
Shreekanth.k <Shreekanth.k000001@gmail.com>
2026-03-20 18:55:58 +0530

    Updated Auction Form

--- app/productAuc/imgUpload/AucSubmit.ts
+++ app/productAuc/imgUpload/AucSubmit.ts
@@ -1,3 +1,4 @@
+"use server"
 import { prisma } from '@/lib//prisma';
 import { redirect } from 'next/navigation'
 import {Category} from '@prisma/client'
@@ -7,26 +8,33 @@ export default async function AucFormSubmit( formData: FormData) {
         const title = String(formData.get('title'));
         const description = String(formData.get('description'));
         const startingBid = Number(formData.get('startingBid'));
-        const startTime = String(formData.get('startTime'));
-        const endTime = String(formData.get('endTime'));
+        const startTime = new Date(String(formData.get('startTime')));
+        const endTime = new Date(String(formData.get('endTime')));
         const category = String(formData.get('category')) as Category;
         const rawimageUrl = String(formData.get('imageUrl'));
+                console.log(rawimageUrl);
         const imageUrl = rawimageUrl ? JSON.parse(rawimageUrl) : [];
         const result = await prisma.productAuction.create({
             data: {
-                'fid': fid,
-                'title': title,
-                'description': description,
-                'startingBid': startingBid,
-                'startTime': startTime,
-                'endTime': endTime,
-                'category': category,
-                'imageUrl': imageUrl,
+                title: title,
+                description: description,
+                startingBid: startingBid,
+                startTime: startTime,
+                endTime: endTime,
+                category: category,
+                imageUrl: imageUrl,
+                user_fid:{
+                    connect:{
+                        uid:fid
+                    }
+                }
             }
         })
+        console.log(result)
     }
     catch (error) {
-        return ;
+        console.log(error)
+        return error;
     }
     redirect('/')
 }
--- app/productAuc/page.tsx
+++ app/productAuc/page.tsx
@@ -6,8 +6,8 @@ import { PhotoIcon } from '@heroicons/react/24/solid'
 import { useUser } from '@/lib/SessionProvider';
 
 export default function AucForm() {
-    const user=useUser();
-    console.log(user);
+    const user = useUser();
+    console.log(user.uid);
     const [imgs, setImgs] = useState<string[]>([]);
     const [imgLimit, setImgLimit] = useState(false);
     const handleImgs = (newUrl: string) => {
@@ -16,13 +16,19 @@ export default function AucForm() {
     }
     useEffect(() => {
         if (imgs.length >= 5) {
+            console.log(imgs.length)
             setImgLimit(true)
         }
-    }), []
+        else {
+            console.log("images length is : " + imgs.length + "and image limit status : " + imgLimit)
+        }
+    }, [imgs])
 
     return (
         <div className='px-6 md:px-[20%] mt-6 pb-10'>
             <form action={AucFormSubmit}>
+                <input type='hidden' name='uid' value={user.uid} />
+                <input type='hidden' name='imageUrl' value={JSON.stringify(imgs)} />
                 <div className="space-y-12">
                     <div className="border-b border-gray-900/10 pb-12">
                         <div className="text-2xl font-semibold text-gray-900">Auction Form</div>
@@ -38,8 +44,8 @@ export default function AucForm() {
                                 <div className="mt-2">
                                     <div className="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-[#009C25]">
                                         <input
-                                            id="prodName"
-                                            name="prodName"
+                                            id="title"
+                                            name="title"
                                             type="text"
                                             placeholder="Apple"
                                             className="block min-w-0 grow bg-white py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
@@ -64,8 +70,81 @@ export default function AucForm() {
                                 <p className="mt-3 text-sm/6 text-gray-600">Write a few sentences about yourself.</p>
                             </div>
 
+                            <div className="sm:col-span-2">
+                                <label htmlFor="prodName" className="block text-sm/6 font-medium text-gray-900">
+                                    Starting Bid Amount
+                                </label>
+                                <div className="mt-2">
+                                    <div className="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-[#009C25]">
+                                        <input
+                                            id="startingBid"
+                                            name="startingBid"
+                                            type="number"
+                                            placeholder="100"
+                                            className="block min-w-0 grow bg-white py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='sm:col-span-4' id='grid-filler1'></div>
 
-                            {imgs.length > 0 ? <div className='col-span-4'>
+                            <div className="sm:col-span-2">
+                                <label htmlFor="prodName" className="block text-sm/6 font-medium text-gray-900">
+                                    Bid Start Time
+                                </label>
+                                <div className="mt-2">
+                                    <div className="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-[#009C25]">
+                                        <input
+                                            id="startTime"
+                                            name="startTime"
+                                            type="date"
+                                            placeholder="Apple"
+                                            className="block min-w-0 grow bg-white py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="sm:col-span-2">
+                                <label htmlFor="prodName" className="block text-sm/6 font-medium text-gray-900">
+                                    Bid End Time
+                                </label>
+                                <div className="mt-2">
+                                    <div className="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-[#009C25]">
+                                        <input
+                                            id="endTime"
+                                            name="endTime"
+                                            type="date"
+                                            placeholder="Apple"
+                                            className="block min-w-0 grow bg-white py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className='sm:col-span-2' id='grid-filler1'></div>
+
+                            <div className="sm:col-span-2">
+                                <label htmlFor="prodName" className="block text-sm/6 font-medium text-gray-900">
+                                    Category
+                                </label>
+                                <div className="mt-2">
+                                    <div className="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-[#009C25]">
+                                        <select className='outline-0' name='category'>
+                                            <option value='VEGETABLES'>Vegetables</option>
+                                            <option value='FRUITS'>Fruits</option>
+                                            <option value='GRAINS'>Grains</option>
+                                            <option value='DAIRY'>Dairy</option>
+                                            <option value='MEAT'>Meat</option>
+                                            <option value='FISH'>Fish</option>
+                                            <option value='OTHER'>Other</option>
+                                        </select>
+
+                                    </div>
+                                </div>
+                            </div>
+
+                            {imgs.length > 0 && (<div className='col-span-4'>
                                 <div className='flex'>
                                     {imgs.map(img =>
                                         <div key={img} className=''>
@@ -73,20 +152,9 @@ export default function AucForm() {
                                         </div>
                                     )}
                                 </div>
-                                { imgLimit ? 
-                                <div className="mt-4 flex text-sm/6 text-gray-600">
-                                    <p
-                                        className="relative cursor-pointer rounded-md bg-transparent font-semibold text-[#009C25] focus-within:outline-2 focus-within:outline-offset-2 focus-within:outline-[#009C25] hover:text-indigo-500"
-                                    >
-                                        <ImgUpload onUploadSuccess={handleImgs} />
-                                    </p>
-                                    <p className=" text-gray-600 pl-1">PNG, JPG, JPEG, WEBP up to 10MB</p>
-                                </div> :
-                                <div>Img Limit Reached: 5 </div>
-                                }
-
                             </div>
-                             
+                            )}
+                            {imgLimit ? <div>Img Limit Reached: 5 </div>
                                 :
                                 <div className="col-span-full">
                                     <p className="block text-sm/6 font-medium text-gray-900">
@@ -108,22 +176,21 @@ export default function AucForm() {
                                     </div>
                                 </div>}
                         </div>
+                        </div>
+
                     </div>
 
-                </div>
-
-                <div className="mt-6 flex items-center justify-end gap-x-6">
-                    <button type="button" className="text-sm/6 font-semibold text-gray-900">
-                        Cancel
-                    </button>
-                    <button
-                        type="submit"
-                        className="rounded-md bg-[#009C25]  px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-green-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#009C25] "
-                    >
-                        Save
-                    </button>
-                </div>
-                {/*  */}
+                    <div className="mt-6 flex items-center justify-end gap-x-6">
+                        <button type="button" className="text-sm/6 font-semibold text-gray-900">
+                            Cancel
+                        </button>
+                        <button
+                            type="submit"
+                            className="rounded-md bg-[#009C25]  px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-green-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#009C25] "
+                        >
+                            Save
+                        </button>
+                    </div>
             </form>
         </div>
     )
